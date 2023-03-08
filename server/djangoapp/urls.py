805fcde3f5e94468dcd21b99103f51aa0534a614
FlorenciaArtucio <florencia.artucio@gmail.com>
2023-03-08 17:32:04 -0500

    User managmente in proces

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,10 +19,11 @@ urlpatterns = [
     path(route='login/', view=views.login_request, name='login'),
     # path for logout
     path(route='logout/', view=views.logout_request, name='logout'),
-    
+     
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-     path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    
     # path for add a review view
     path(route='dealer/<int:dealer_id>/add_review', view=views.add_review, name='add_review')
 
